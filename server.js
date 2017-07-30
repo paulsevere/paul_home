e71e6305547b0b51042b53515b94a85425c6dfc4
paul <paul@pauls-MacBook-Pro.local>
2017-07-29 23:01:14 -0400

    fixed favicon

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const http = require("http");
 const fs = require("fs");
 
 const index = fs.readFileSync("static/index.html", "utf-8");
+const favicon = fs.readFileSync("static/favicon.ico");
 
 http
   .createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+      return res.end(favicon);
+    }
     res.end(index);
   })
   .listen(80);
